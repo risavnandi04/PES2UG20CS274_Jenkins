78bf32ae64779a5e0f1216bc516219447c3b56a3
risavnandi04 <79854041+risavnandi04@users.noreply.github.com>
2023-02-26 12:11:45 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(void) {
-  cout << "I'm Awesome\n";
+  cout << "BOOOO\n";
 //   cout << "Hello, Jenkins\n";
 //   cout << "I have successfully built and run\n";
   return 0;
